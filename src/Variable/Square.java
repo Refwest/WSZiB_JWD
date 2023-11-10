974b9b88d7af6fd42d99c2549b54cdef6c39a796
Filip Działa <fi.dziala@gmail.com>
2023-11-10 23:43:07 +0100

    Laboratory3 - variable - square parameters

---
+++ src/Variable/Square.java
@@ -0,0 +1,27 @@
+package Variable;
+
+public class Square {
+    //    Napisz program obliczający pole powierzchni, obwód oraz długość przekątnej dla kwadratów o następujących długościach boku: 2, 7, 11 oraz 19.
+    public static void main(String[] args) {
+        int square1 = 2;
+        int square2 = 7;
+        int square3 = 11;
+        int square4 = 19;
+
+        sqParameters(square1);
+        sqParameters(square2);
+        sqParameters(square3);
+        sqParameters(square4);
+    }
+
+    public static void sqParameters(int a) {
+        System.out.println("Kwadrat o boku: " + a + " ma: ");
+        System.out.println("pole powierzchni równe: " + a * a);
+        System.out.println("obwód równy: " + (4 * a));
+        System.out.printf("przekątną o długości: %.2f%n", (a * Math.sqrt(a)));
+        System.out.println("-----------------------------------------");
+    }
+
+}
+
+
