158fab860534aca3a0145feb1e4ccefbf2ee691b
Filip Działa <fi.dziala@gmail.com>
2024-01-03 21:01:31 +0100

    Overloading

--- src/Overloading/lab10/Calculator.java
+++ src/Overloading/lab10/Calculator.java
@@ -1,4 +1,6 @@
 package Overloading.lab10;
+//F
+
 /*Napisz program Calculator posiadający metody o nazwie add sumujące 2 lub 3 liczby typu int lub double.
 Przykład
 
@@ -7,15 +9,30 @@ int res1 = calc.add(1, 2);
 double res2 = calc.add(1.0, 2.4, 3.4);*/
 
 public class Calculator {
-
     public static void main(String[] args) {
-        add(5,126);
-    }
-    public static void add(int a, int b) {
-        System.out.println("Suma liczb równa się: " + (a + b));
+        Calculator calc = new Calculator();
+        double result = calc.add(5.5, 126, 1111.2);
+        int result2 = calc.add(2,2);
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(calc.add(1,1,222));
     }
 
-    public static void add(int a, int b, int c) {
-        System.out.println("Suma liczb równa się: " + (a + b + c));
+    public int add(int a, int b) {
+
+        return (a + b);
+    }
+
+    public int add(int a, int b, int c) {
+        return (a + b + c);
+    }
+
+    public double add(double a, double b) {
+        return (a + b);
+    }
+
+    public double add(double a, double b, double c) {
+
+        return (a + b + c);
     }
 }
